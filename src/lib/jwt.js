9c87e2cc8d1a420cd1f3ae1428082e31f48e71be
황진수-PC <cjswp122@gmail.com>
2018-01-24 17:56:28 +0900

    make jwt

---
+++ src/lib/jwt.js
@@ -0,0 +1,23 @@
+// @flow
+import jwt from 'jsonwebtoken';
+
+const { JWT_SECRET: secret } = process.env;
+
+export const generate = (payload: any, options: any): Promise<string> =>
+  new Promise((resolve, reject) => {
+    jwt.sign(payload, secret, {
+      issuer: 'jeckson.io',
+      expiresIn: '7d',
+      ...options,
+    }, (err, token) => {
+      if (err) reject(err);
+      resolve(token);
+    });
+  });
+
+export const decode = (token: string): Promise<any> => new Promise((resolve, reject) => {
+  jwt.verify(token, secret, (err, decoded) => {
+    if (err) reject(err);
+    resolve(decoded);
+  });
+});
