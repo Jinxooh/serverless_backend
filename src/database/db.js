43bde9a4caa88a48e4875189791e0937d74809be
황진수-PC <cjswp122@gmail.com>
2018-05-21 16:57:38 +0900

    for local DB!!

--- src/database/db.js
+++ src/database/db.js
@@ -2,9 +2,18 @@
 import SequelizeCockroach from 'sequelize-cockroachdb';
 import type Sequelize from 'sequelize';
 
-const { COCKROACHDB_HOST, COCKROACHDB_PW } = process.env;
-const db:Sequelize = new SequelizeCockroach('jeckson', 'jeckson', COCKROACHDB_PW, {
-  host: COCKROACHDB_HOST,
+const {
+  COCKROACHDB_HOST,
+  COCKROACHDB_PW,
+  LOCAL_DB_HOST,
+  LOCAL_PW,
+} = process.env;
+const { APP_ENV } = process.env;
+
+const host = APP_ENV === 'local' ? LOCAL_DB_HOST : COCKROACHDB_HOST;
+const pw = APP_ENV === 'local' ? LOCAL_PW : COCKROACHDB_PW;
+const db:Sequelize = new SequelizeCockroach('jeckson', 'jeckson', pw, {
+  host,
   dialect: 'postgres',
   port: 26257,
   loggin: true,
