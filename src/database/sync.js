da356601e5bf6c52362c201cbf003eb12c671a78
jinxooh <cjswp122@gmail.com>
2018-05-29 00:00:09 +0900

    sync add

--- src/database/sync.js
+++ src/database/sync.js
@@ -3,16 +3,33 @@ import {
   SocialAccount,
   User,
   UserProfile,
+  Post,
+  Category,
+  PostsCategories,
+  Tags,
+  PostsTags,
 } from './models';
 
 export default function sync() {
   // configure relation
   UserProfile.associate();
   SocialAccount.associate();
+  Post.associate();
+  Category.associate();
+  PostsCategories.associate();
+  PostsTags.associate();
 
+  if (process.env.SYNC_DB !== 'true') {
+    return;
+  }
   // sync Models
   User.sync();
   UserProfile.sync();
   SocialAccount.sync();
   EmailAuth.sync();
+  Post.sync();
+  Category.sync();
+  PostsCategories.sync();
+  Tags.sync();
+  PostsTags.sync();
 }
