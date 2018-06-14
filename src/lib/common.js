f5f874fda2aeba7c54738ede9859ef9faee7fdc3
황진수-PC <cjswp122@gmail.com>
2018-06-14 15:58:30 +0900

    category api delete rename

--- src/lib/common.js
+++ src/lib/common.js
@@ -1,5 +1,5 @@
 // @flow
-import type { Context } from 'koa';
+import type { Middleware } from 'koa';
 import Sequelize from 'sequelize';
 import Joi from 'joi';
 
@@ -10,7 +10,7 @@ export const primaryUUID = {
 };
 
 // validates schema, return 400 error if not valid
-export const validateSchema = (ctx: Context, schema: any) => {
+export const validateSchema = (ctx: any, schema: any) => {
   const result = Joi.validate(ctx.request.body, schema);
   if (result.error) {
     ctx.status = 400;
@@ -25,4 +25,22 @@ export const validateSchema = (ctx: Context, schema: any) => {
 
 export const filterUnique = (array: Array<*>): Array<*> => {
   return [...new Set(array)]; // check Duplication
+};
+
+export const isUUID = (name: string) => {
+  const validation = Joi.validate(name, Joi.string().uuid());
+  if (validation.error) return false;
+  return true;
+};
+
+export const checkUUID: Middleware = (ctx, next) => {
+  const { id } = ctx.params;
+  if (!isUUID(id)) {
+    ctx.status = 400;
+    ctx.body = {
+      name: 'NOT_UUID',
+    };
+    return;
+  }
+  return next();
 };
--- src/router/me/categories/categories.ctrl.js
+++ src/router/me/categories/categories.ctrl.js
@@ -1,7 +1,7 @@
 // @flow
 import type { Context } from 'koa';
 import Joi from 'joi';
-import { validateSchema } from 'lib/common';
+import { validateSchema, isUUID } from 'lib/common';
 import { Category } from 'database/models';
 
 export const listCategories = async (ctx: Context): Promise<*> => {
@@ -42,6 +42,69 @@ export const createCategory = async (ctx: Context): Promise<*> => {
 };
 
 export const renameCategory = async (ctx: Context): Promise<*> => {
-  const { id } = ctx.user;
+  const { id } = ctx.params;
+  if (!isUUID(id)) {
+    ctx.status = 400;
+    ctx.body = {
+      name: 'NOT_UUID',
+    };
+    return;
+  }
+
+  type BodySchema = {
+    name: string
+  };
+
+  const schema = Joi.object().keys({
+    name: Joi.string().required().min(1).max(40),
+  });
+
+  if (!validateSchema(ctx, schema)) return;
+
+  const { id: userId } = ctx.user;
+  const { name }: BodySchema = (ctx.request.body: any);
+
   console.log('id', id);
+  const category = await Category.findOne({
+    attributes: ['name', 'id', 'parent', 'order', 'private'],
+    where: {
+      id,
+      fk_user_id: userId,
+    },
+  });
+
+  if (!category) {
+    ctx.status = 404;
+    return;
+  }
+
+  category.name = name;
+  await category.save();
+
+  ctx.body = category.toJSON();
+};
+
+export const deleteCategory = async (ctx: Context): Promise<*> => {
+  const { id } = ctx.params;
+  const { id: userId } = ctx.user;
+
+  const category = await Category.findOne({
+    attributes: ['id', 'order', 'parent', 'private', 'name'],
+    where: {
+      id,
+      fk_user_id: userId,
+    },
+  });
+
+  if (!category) {
+    ctx.status = 404;
+    return;
+  }
+
+  // try {
+  //   await category.destory();
+  //   await Category.update({
+  //     order: literal('"order" - 1'),
+  //   })
+  // }
 };
