dc24f6ab08a4fcc075e199179cd9409faa2f46d3
황진수-PC <cjswp122@gmail.com>
2018-07-04 10:19:20 +0900

    Post Like API Implement

--- src/router/posts/index.js
+++ src/router/posts/index.js
@@ -1,8 +1,11 @@
 // @flow
 import Router from 'koa-router';
 import needsAuth from 'lib/middlewares/needsAuth';
+import { checkUUID } from 'lib/common';
+import post from './post';
 
 import * as postsCtrl from './posts.ctrl';
+import { checkPostExistancy } from './post.ctrl';
 
 const posts: Router = new Router();
 
@@ -10,5 +13,6 @@ posts.post('/', needsAuth, postsCtrl.writePost);
 posts.get('/@:username/:urlSlug', postsCtrl.readPost); // @ + username/urlSlug
 posts.get('/@:username', postsCtrl.listPosts);
 posts.get('/public', postsCtrl.listPosts);
+posts.use('/:id', checkUUID, checkPostExistancy, post.routes());
 
 export default posts;
